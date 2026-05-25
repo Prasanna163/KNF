a4b978ba41d67cb1113883bf58886cff044d88c6
Prasanna163 <prasannakulkarni163@gmail.com>
2026-05-25 11:38:31 +0530

    Set package name to knf for PyPI release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='nciforge',
+    name='knf',
     version='1.0.8',
     description='Automated Descriptor Engine for SNCI, SCDI, and 9D KNF',
     long_description=long_description,
